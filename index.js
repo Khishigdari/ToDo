cce7ada6dbd423bc7fccabbf2fdcbdd2ce9966a3
Khishigdari <nbhishgee22@gmail.com>
2025-08-07 11:53:37 +0800

    x button added

---
+++ index.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body");
+const input = document.createElement("input");
+const button = document.createElement("button");
+const ul = document.createElement("ul");
+
+button.innerText = "add";
+
+body.appendChild(input);
+body.appendChild(button);
+body.appendChild(ul);
+
+const data = [];
+
+const addTodo = (value) => {
+  data.push(value);
+};
+
+const createElement = (value) => {
+  const li = document.createElement("li");
+  const x = document.createElement("button");
+  li.innerText = value;
+  x.innerText = "x";
+  li.appendChild(x);
+  ul.appendChild(li);
+};
+
+const render = () => {
+  ul.innerHTML = "";
+  data.map((value) => {
+    createElement(value);
+  });
+};
+
+button.addEventListener("click", () => {
+  addTodo(input.value);
+  render();
+});
